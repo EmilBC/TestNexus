78f12800a53bd2c915e85aaf5ec9810bbc2c1408
EmilBC <135125992+EmilBC@users.noreply.github.com>
2023-07-07 11:40:46 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildApp(){
 
 def pushImage(){
     echo 'Building Image ...'
-     sh "docker build -t  https://03a8-2a01-cb06-b031-4ee1-d847-9253-6f0c-4e21.ngrok-free.app/devopsexamplenew1234567:${env.BUILD_NUMBER} ."
+     sh "docker -H https://03a8-2a01-cb06-b031-4ee1-d847-9253-6f0c-4e21.ngrok-free.app build -t  devopsexamplenew1234567:${env.BUILD_NUMBER} ."
 
     echo 'Pushing image to docker hosted rerpository on Nexus'
 
