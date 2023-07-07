acd87e4a80390a23202e02c907e70e5204ecb73c
EmilBC <135125992+EmilBC@users.noreply.github.com>
2023-07-07 11:22:09 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -11,7 +11,7 @@ def pushImage(){
 
 withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'PSW', usernameVariable: 'USER')]){
         sh "echo ${PSW} | docker login -u ${USER} --password-stdin  https://03a8-2a01-cb06-b031-4ee1-d847-9253-6f0c-4e21.ngrok-free.app"
-        sh "docker push https://03a8-2a01-cb06-b031-4ee1-d847-9253-6f0c-4e21.ngrok-free.app/devopsexamplenew1234567:${env.BUILD_NUMBER}"
+        //sh "docker push https://03a8-2a01-cb06-b031-4ee1-d847-9253-6f0c-4e21.ngrok-free.app/devopsexamplenew1234567:${env.BUILD_NUMBER}"
     }
 }
 
